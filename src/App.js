6e73fb2bc060bbd7ce3ac85d7ea5d9a88b4db04f
osakoh <macmive@gmail.com>
2021-06-22 14:09:07 +0100

    updated expense list

--- src/App.js
+++ src/App.js
@@ -6,22 +6,34 @@ import NewExpense from "./components/NewExpense/NewExpense";
 const initialExpenses = [
   {
     id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
+    title: "Gym Membership",
+    amount: 24.12,
+    date: new Date(2019, 8, 14),
   },
   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: "e3",
     title: "Car Insurance",
     amount: 294.67,
-    date: new Date(2021, 2, 28),
+    date: new Date(2019, 2, 28),
   },
   {
     id: "e4",
-    title: "New Desk (Wooden)",
-    amount: 450,
-    date: new Date(2021, 5, 12),
+    title: "Uplift Desk",
+    amount: 199.3,
+    date: new Date(2020, 5, 12),
+  },
+  {
+    id: "e5",
+    title: "Laptop and Monitor",
+    amount: 899.49,
+    date: new Date(2020, 20, 12),
+  },
+  {
+    id: "e6",
+    title: "Council tax",
+    amount: 250.49,
+    date: new Date(2021, 2, 12),
   },
 ];
 
